3e88160d98ba8897c3b980657949d47b7ff7a97a
Nikola1308 <jovanovicnikola1308@gmail.com>
2019-07-02 09:01:53 +0200

    Endpoint for reading Tasks and single Task

--- src/index.js
+++ src/index.js
@@ -7,13 +7,36 @@ const port = process.env.PORT || 3001
 
 app.use(express.json())
 
+//Endpoint post for posting Tasks
 app.post('/todos',(req,res)=>{
     const todo = new ToDoList(req.body)
 
     todo.save().then(()=>{
-        res.send(todo)
-    }).catch(()=>{
-        
+        res.status(201).send(todo)
+    }).catch((e)=>{
+        res.status(400).res.send()
+    })
+})
+
+//Endpoint for reading Tasks
+app.get('/todos',(req,res)=>{
+    ToDoList.find({typeOfCard:'TaskToDo'}).then((tasks)=>{
+        res.send(tasks)
+    }).catch((e)=>{
+        res.status(500).res.send()
+    })
+})
+//Endpoint for reading Tasks 
+app.get('/todos/:id',(req,res)=>{
+    const _id =  req.params.id
+    ToDoList.findById(_id).then((task)=>{
+        if(!task){
+            return res.status(404).send()
+        }
+        res.send(task)
+    }).catch((e)=>{
+        res.status(500).send()
+
     })
 })
 
--- src/models/ToDoList.js
+++ src/models/ToDoList.js
@@ -2,18 +2,12 @@ const mongoose = require('mongoose')
 
 const TodoList = mongoose.model('TodoList',{
     value:{
-        type: String
+        type:String
     },
-    taksToDo:{
-        type:Boolean
-    },
-    taskDone:{
-        type:Boolean
-    },
-    taskDeleted:{
-        type:Boolean
+    typeOfCard:{
+        type:String,
+        enum:['TaskToDo','TaskDone','TaskDeleted']
     }
-
 })
 
 

e4605c4a3a88d33ba4e5a970aaaa17565831e4ce
Nikola1308 <jovanovicnikola1308@gmail.com>
2019-07-02 13:15:28 +0200

    Endpoints for toggling tasks

--- src/index.js
+++ src/index.js
@@ -8,37 +8,132 @@ const port = process.env.PORT || 3001
 app.use(express.json())
 
 //Endpoint post for posting Tasks
-app.post('/todos',(req,res)=>{
+app.post('/todos', async (req,res)=>{
     const todo = new ToDoList(req.body)
-
-    todo.save().then(()=>{
+    try{
+        await todo.save()
         res.status(201).send(todo)
-    }).catch((e)=>{
-        res.status(400).res.send()
-    })
+    }catch(e){
+        res.status(400).send(e)
+    }
 })
 
-//Endpoint for reading Tasks
-app.get('/todos',(req,res)=>{
-    ToDoList.find({typeOfCard:'TaskToDo'}).then((tasks)=>{
+//Endpoint for reading all Tasks
+app.get('/todos',async(req,res)=>{
+    try{
+        const tasks = await ToDoList.find({})
         res.send(tasks)
-    }).catch((e)=>{
-        res.status(500).res.send()
-    })
+    }catch(e){
+        res.status(500).send(e)
+    }
 })
-//Endpoint for reading Tasks 
-app.get('/todos/:id',(req,res)=>{
-    const _id =  req.params.id
-    ToDoList.findById(_id).then((task)=>{
+
+//Endpoint for reading Tasks of TaskToDo by type
+app.get('/todos/tasktodo',async(req,res)=>{
+    try{
+        const task = await ToDoList.find({typeOfCard:'TaskToDo'})
         if(!task){
             return res.status(404).send()
         }
         res.send(task)
-    }).catch((e)=>{
-        res.status(500).send()
-
-    })
+    }catch(e){
+        res.status(500).send(e)
+    }
+}) 
+//Endpoint for reading Tasks of TaskDone by type
+app.get('/todos/taskdone',async(req,res)=>{
+    try{
+        const task = await ToDoList.find({typeOfCard:'TaskDone'})
+        if(!task){
+            return res.status(404).send()
+        }
+        res.send(task)
+    }catch(e){
+        res.status(500).send(e)
+    }
 })
+//Endpoint for reading Task of TaskDeleted by type
+app.get('/todos/taskdeleted', async(req,res)=>{    
+    try{
+        const task = await ToDoList.find({typeOfCard:'TaskDeleted'})
+        if(!task){
+            return res.status(404).send()
+        }
+        res.send(task)
+    }catch(e){
+        res.status(500).send(e)
+    }
+})
+//Endpoint for reading any Task by id 
+app.get('/todos/tasks/:id',async(req,res)=>{
+    const _id =  req.params.id
+    try{
+        const task = await ToDoList.findById(_id)
+        if(!task){
+            return res.status(404).send()
+        }
+        res.send(task)
+    }catch(e){
+        res.status(500).send()
+    }
+})
+//Endpoint for updating TypeOfCard-from ToDoTask to DoneTask
+app.patch('/todos/tasks/:id',async(req,res)=>{
+    const _id = req.params.id
+    try{
+        const task = await ToDoList.findByIdAndUpdate(_id,{typeOfCard:'TaskDone'},{new:true,runValidators:true})
+        if(!task){
+            return res.status(404).send()
+        }
+        res.send(task)
+    }catch(e){
+        res.status(400).send(e)
+    }
+})
+//Endpoint for updating TypeOfCard-from DoneTask to TaskDeleted
+app.patch('/todos/tasksdonetotaskdelete/:id',async(req,res)=>{
+    const _id = req.params.id
+    try{
+        const task = await ToDoList.findByIdAndUpdate(_id,{typeOfCard:'TaskDeleted'},{new:true,runValidators:true})
+        if(!task){
+            return res.status(404).send()
+        }
+        res.send(task)
+    }catch(e){
+        res.status(400).send()
+    }
+})
+//Endpoint for updating TypeOfCard-from DoneTask to ToDoTask
+app.patch('/todos/taskdonetotask/:id',async(req,res)=>{
+    const _id = req.params.id
+    try{
+        const task = await ToDoList.findByIdAndUpdate(_id,{typeOfCard:'TaskToDo'},{new:true,runValidators:true})
+        if(!task){
+            return res.status(404).send()
+        }
+        res.send(task)
+    }catch(e){
+        res.status(400).send()
+    }
+})
+//Endpoint for updating TypeOfCard-from TaskDeleted to DoneTask
+app.patch('/todos/taskdeletedtotaskdone/:id',async(req,res)=>{
+    const _id = req.params.id
+    try{
+        const task = await ToDoList.findByIdAndUpdate(_id,{typeOfCard:'TaskDone'},{new:true,runValidators:true})
+        if(!task){
+            return res.status(404).send()
+        }
+        res.send(task)
+    }catch(e){
+        res.status(400).send()
+    }
+})
+
+//Endpoint for deleting from ToDoTask
+//Endpoint for deleting from TaskDeleted
+
+
 
 app.listen(port,()=>{
     console.log('Serve is up on port: ' + port)
--- src/mongodb/mongoose.js
+++ src/mongodb/mongoose.js
@@ -3,5 +3,6 @@ const mongoose = require('mongoose')
 //Connection to DB - MongoDB
 mongoose.connect('mongodb://127.0.0.1:27017/todo-list',{
     useNewUrlParser:true,
-    useCreateIndex:true
+    useCreateIndex:true,
+    useFindAndModify:false
 })
